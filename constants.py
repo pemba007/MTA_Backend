4efc66e7c3a1abe50c68c70c19288d06a954a209
Pemba Sherpa <pemba_sherpa64@yahoo.com>
2021-12-08 14:01:01 -0500

    Final Commit for deployment

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
-DB_HOST = "127.0.0.1"
-DB_USER = 'postgres'
-DB_PASSWORD = '    '
-DB_PORT = '5432'
-DB_DATABASE = 'MTA_Test'
-
-# def hostingAddress = "127.0.0.1"
-
-# conn = psycopg2.connect(database="MTA_Test",
-#                         user='postgres',
-#                         password='    ',
-#                         host='127.0.0.1',
-#                         port='5432')
+import os
+if os.environ['ENV'] == 'prod':
+    DB_HOST = "john.db.elephantsql.com"
+    DB_USER = 'tsetxsix'
+    DB_PASSWORD = 'mXamL8z02zo5a5f-4QkS1qyjeblPZxy8'
+    DB_PORT = '5432'
+    DB_DATABASE = 'tsetxsix'
+else:
+    DB_HOST = "127.0.0.1"
+    DB_USER = 'postgres'
+    DB_PASSWORD = '    '
+    DB_PORT = '5432'
+    DB_DATABASE = 'MTA_Test'
