966716bbac4a8d39585a3bd7d3135635d440991d
Pemba Sherpa <pemba_sherpa64@yahoo.com>
2021-12-09 19:10:34 -0500

    Changes for deployment

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 import os
-if os.environ['ENV'] == 'prod':
-    DB_HOST = "john.db.elephantsql.com"
-    DB_USER = 'tsetxsix'
-    DB_PASSWORD = 'mXamL8z02zo5a5f-4QkS1qyjeblPZxy8'
-    DB_PORT = '5432'
-    DB_DATABASE = 'tsetxsix'
-else:
-    DB_HOST = "127.0.0.1"
-    DB_USER = 'postgres'
-    DB_PASSWORD = '    '
-    DB_PORT = '5432'
-    DB_DATABASE = 'MTA_Test'
+# if os.environ['ENV'] == 'prod':
+# DB_HOST = "john.db.elephantsql.com"
+# DB_USER = 'tsetxsix'
+# DB_PASSWORD = 'mXamL8z02zo5a5f-4QkS1qyjeblPZxy8'
+# DB_PORT = '5432'
+# DB_DATABASE = 'tsetxsix'
+# else:
+DB_HOST = "127.0.0.1"
+DB_USER = 'postgres'
+DB_PASSWORD = '    '
+DB_PORT = '5432'
+DB_DATABASE = 'MTA_Test'
